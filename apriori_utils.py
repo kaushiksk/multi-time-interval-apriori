be921f7b2dcb26a816a8e9ba829ebc0a3877540a
kaushiksk <kaushikskalmady@gmail.com>
2018-11-11 19:56:50 +0530

    Adds some template code

---
+++ apriori_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : apriori_utils.py
+# Author            : Kaushik S Kalmady
+# Date              : 11.11.2018
+# Last Modified Date: 11.11.2018
+# Last Modified By  : Kaushik S Kalmady
+
+
+def contains(sequenceList, sequence):
+    """Returns True is sequence is contained in sequenceList as per multi-time
+    interval sequence's "contains" definition.
+
+    Arguments:
+        sequenceList: List of Multi Time Interval Sequences
+        sequence: Sequence to check for contains clause
+    """
+
+    pass
+
---
+++ transaction.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : transaction.py
+# Author            : Kaushik S Kalmady
+# Date              : 11.11.2018
+# Last Modified Date: 11.11.2018
+# Last Modified By  : Kaushik S Kalmady
+
+
+class Transaction:
+    """Implements the Transaction Class.
+    This defines a Transaction object that is used to represent transactions in
+    a multi time interval sequence database."""
+
+    def __init__():
+        pass
+
+
+
+class Sequence:
+    """This is a collection o multi time interval sequence"
+
+    def __init__():
+        pass
+
+
