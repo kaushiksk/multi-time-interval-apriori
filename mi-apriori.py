bed9f405e61724d84558e0060fc41636de84b8a7
siddharthvdn <siddharthvdn@gmail.com>
2018-11-21 23:27:53 +0530

    Add stub funtion to run apriori for given sequence length

--- mi-apriori.py
+++ mi-apriori.py
@@ -13,9 +13,18 @@ Implements the multi time interval apriori algorithm
 from interval import make_table, TIME_INTERVALS
 from apriori_utils import joinC2, joinCk, contains, generate_one_itemsets
 from apriori_utils import support, one_item_support
+from apriori_utils import run_apriori
 from config import TIME_INTERVALS, MIN_SUP, DB
 
+import sys
 
 time_interval_matrix = make_table(TIME_INTERVALS)
 one_itemsets = generate_one_itemsets(DB, MIN_SUP)
 
+
+if __name__=="__main__":
+
+	max_sequence_length = sys.argv[1]
+
+
+	frequent_sequences = run_apriori(DB, TIME_INTERVALS, max_sequence_length, MIN_SUP)
